740808d180d63d670550b103c956c7ac0b32d242
-.- <delciopolanco@gmail.com>
2020-05-20 02:57:45 -0400

    Actualizando countdounw

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,30 @@ $(window).load(function () {
 });
 
 $(document).ready(function () {
-  $("#counter").countdown({
-    until: new Date(2020, 10 - 1, 12),
-    format: 'dHMS'
+  $("#counter").countdown('2020/09/01').on('update.countdown', function (event) {
+    var $this = $(this).html(event.strftime('' +
+      '<span class = "countdown-row countdown-show4">' +
+      '<span class = "countdown-section">' +
+      '<span class = "countdown-amount">%-D</span>' +
+      '<span class = "countdown-period">Dias</span>' +
+      '</span>' +
+      '<span class = "countdown-section">' +
+      '<span class = "countdown-amount">%-H</span>' +
+      '<span class = "countdown-period">Horas</span>' +
+      '</span>' +
+      '<span class = "countdown-section">' +
+      '<span class = "countdown-amount">%-M</span>' +
+      '<span class = "countdown-period">Minutos</span>' +
+      '</span>' +
+      '<span class = "countdown-section">' +
+      '<span class = "countdown-amount">%-S</span>' +
+      '<span class = "countdown-period">Segundos</span>' +
+      '</span>' +
+      '</span>'
+    ));
   });
+
+
 
   $("#counter_wrapper").fitText(1.2, {
     minFontSize: '20px',
